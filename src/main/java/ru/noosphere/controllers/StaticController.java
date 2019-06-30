a3079de67eb052ce4bcc724cac063711a2132ae5
vmstukalov <vmstukalov@gmail.com>
2019-06-30 14:19:41 +0300

    Добавлены web.xml и главная страница

---
+++ src/main/java/ru/noosphere/controllers/StaticController.java
@@ -0,0 +1,16 @@
+package ru.noosphere.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class StaticController {
+
+    @GetMapping
+    public String index() {
+        return "static/index";
+    }
+
+}
